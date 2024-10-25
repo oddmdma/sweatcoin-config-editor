70fa61cb56f998ad107e4cca99526dbf7a3a76ab
Sergey Burukhin <oddmdma@gmail.com>
2024-10-25 15:55:20 +0100

    Add MainPage and refactor BASE_URL export
    Introduce a MainPage component and update App.tsx to include its route. Refactor the BASE_URL constant into a separate file for better manageability. Additionally, correct case inconsistencies in file names for BoostBurst and BrandedBoost components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import {BoostBurst} from "./pages/boost-burst";
-import {BrandedBoost} from "./pages/branded-boost";
+import {BoostBurst} from "./pages/BoostBurst";
+import {BrandedBoost} from "./pages/BrandedBoost";
+import {BASE_URL} from "./constants";
+import {MainPage} from "./pages/Main";
 
-const BASE_URL = '/sweatcoin-config-editor'
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
+				<Route path={BASE_URL} element={<MainPage/>}/>
 				<Route path={`${BASE_URL}/boost-burst`} element={<BoostBurst/>}/>
 				<Route path={`${BASE_URL}/branded-boost`} element={<BrandedBoost/>}/>
 			</Routes>
