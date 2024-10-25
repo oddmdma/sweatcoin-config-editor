a236f6cbb74291ac093dba728bf82677f4116096
Sergey Burukhin <oddmdma@gmail.com>
2024-10-25 16:05:36 +0100

    Refactor component structure and update default configs
    Replaced list with Box components for links in MainPage. Adjusted default configuration values in BrandedBoost to use undefined instead of 0 for uninitialized properties. This improves clarity and consistency across components.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {BASE_URL} from "../constants";
-import {Container, Heading, Section} from "@radix-ui/themes";
+import {Box, Container, Heading, Section} from "@radix-ui/themes";
 
 export const MainPage: React.FC = () => {
 	return (
 		<Container px="8" size="3" width="100%">
 			<Section size="1">
 				<Heading as="h1" mb="7">Main Page</Heading>
-				<ul>
-					<li><Link to={`${BASE_URL}/boost-burst`}>Boost Burst</Link></li>
-					<li><Link to={`${BASE_URL}/branded-boost`}>Branded Boost</Link></li>
-				</ul>
+				<Box><Link to={`${BASE_URL}/boost-burst`}>Boost Burst</Link></Box>
+				<Box><Link to={`${BASE_URL}/branded-boost`}>Branded Boost</Link></Box>
 			</Section>
 		</Container>
 	);
